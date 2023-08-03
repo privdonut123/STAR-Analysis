11a592d6a16890e9037fe7e10b3cf019815ea1b1
privdonut123@ <privdonut123@gmail.com>
2023-08-02 20:29:05 -0400

    Adding environment setup script as well as missing file from StRoot

---
+++ env.sh
@@ -0,0 +1,8 @@
+module purge
+unset MODULEPATH
+setup 64b
+starver SL23x config/v0.2.3-rhel7-root5.34.38-64b
+
+# setup 32b
+# starver SL23x config/v0.2.3-rhel7-root5.34.38-32b
+#module load zlib-1.2.12
